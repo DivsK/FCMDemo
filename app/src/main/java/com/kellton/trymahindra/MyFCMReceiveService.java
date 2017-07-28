69fb39a76956ed5e9194128b7dd6e4e38b203564
Divya <divya.Khanduri@kelltontech.com>
2017-07-28 16:41:35 +0530

    +added broadcast

--- app/src/main/java/com/kellton/trymahindra/MyFCMReceiveService.java
+++ app/src/main/java/com/kellton/trymahindra/MyFCMReceiveService.java
@@ -7,8 +7,8 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
-import android.widget.RemoteViews;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -18,19 +18,18 @@ import com.google.firebase.messaging.RemoteMessage;
  */
 
 public class MyFCMReceiveService extends FirebaseMessagingService {
-    public final String TAG = "FROM--->";
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         Log.e("hello","onMessageReceived");
         //Displaying data in log
         //It is optional
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
-        Log.d("msgBody--", "Notification Message Body: " + remoteMessage.getNotification().getBody());
+        Log.d("Message--", "From: " + remoteMessage.getFrom());
+        Log.d("Message Body--", "Notification Message Body: " + remoteMessage.getNotification().getBody());
 
         //Calling method to generate notification
         //sendNotification(remoteMessage.getNotification().getBody(), remoteMessage.getData().get("Name").toString(), remoteMessage.getData().get("Phone-No").toString());
-        createNotification();
+        sendBroadcast(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
     }
 
     private void sendNotification(String body, String name, String phone) {
@@ -59,46 +58,13 @@ public class MyFCMReceiveService extends FirebaseMessagingService {
         notificationManager.notify(0, notificationBuilder.build());
     }
 
-    private void createNotification() {
-        Log.e("hello","onMessageReceived");
-
-        Intent intent = new Intent(this, NotificationActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
-
-
-        RemoteViews contentView = new RemoteViews(getPackageName(), R.layout.custom_notification);
-
-        //Dial intent
-        Intent dial = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:09882527868"));
-        dial.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        PendingIntent pendingIntentDial = PendingIntent.getActivity(this, 0, dial, PendingIntent.FLAG_UPDATE_CURRENT);
-        contentView.setOnClickPendingIntent(R.id.btn_call, pendingIntentDial);
-
-        //Message intent
-        Uri uri = Uri.parse("smsto:09882527868");
-        Intent message = new Intent(Intent.ACTION_SENDTO, uri);
-        message.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        PendingIntent pendingIntentMessage = PendingIntent.getActivity(this, 0, message, PendingIntent.FLAG_UPDATE_CURRENT);
-        contentView.setOnClickPendingIntent(R.id.btn_message, pendingIntentMessage);
-
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-
-                .setSmallIcon(R.drawable.notification)
-                .setTicker("custom notification")
-                .setAutoCancel(true)
-                .setContentIntent(pendingIntent)
-                .setCustomBigContentView(contentView);
-
-        contentView.setTextViewText(R.id.tv_title, "title");
-        contentView.setTextViewText(R.id.tv_text, "text");
-        contentView.setImageViewResource(R.id.iv_icon, R.drawable.notification);
-
-
-        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        notificationManager.notify(0, builder.build());
+    public  void sendBroadcast(String title,String message)
+    {
+        Intent intent=new Intent("com.kellton.trymahindra_FCM-MESSAGE");
+        intent.putExtra("title",title);
+        intent.putExtra("message",message);
+        LocalBroadcastManager localBroadcastManager=LocalBroadcastManager.getInstance(this);
+        localBroadcastManager.sendBroadcast(intent);
 
     }
 }
--- app/src/main/java/com/kellton/trymahindra/NotificationActivity.java
+++ app/src/main/java/com/kellton/trymahindra/NotificationActivity.java
@@ -1,22 +1,84 @@
 package com.kellton.trymahindra;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
+import android.util.Log;
+import android.widget.RemoteViews;
 import android.widget.TextView;
 
 public class NotificationActivity extends AppCompatActivity {
     private TextView mTextView;
-    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiverHandler,new IntentFilter("com.kellton.trymahindra_FCM-MESSAGE"));
         setContentView(R.layout.activity_notification);
         mTextView = (TextView) findViewById(R.id.textView);
-        mButton = (Button) findViewById(R.id.button);
-        mTextView.setText("NAME " + getIntent().getStringExtra("name") + "\n" + "PHONE NO" + getIntent().getStringExtra("phone"));
+        if(getIntent().getExtras()!=null)
+        {
+            mTextView.setText("Title" + getIntent().getStringExtra("title") + "\n" + "Message" + getIntent().getStringExtra("message"));
+
+        }
     }
 
+    private void createNotification(String title, String body, Context context) {
+        Log.e("hello","onMessageReceived");
+        Intent intent = new Intent(getApplicationContext(), NotificationActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
 
+
+        RemoteViews contentView = new RemoteViews(getPackageName(), R.layout.custom_notification);
+
+        //Dial intent
+        Intent dial = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:09882527868"));
+        dial.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        PendingIntent pendingIntentDial = PendingIntent.getActivity(this, 0, dial, PendingIntent.FLAG_UPDATE_CURRENT);
+        contentView.setOnClickPendingIntent(R.id.btn_call, pendingIntentDial);
+
+        //Message intent
+        Uri uri = Uri.parse("smsto:09882527868");
+        Intent message = new Intent(Intent.ACTION_SENDTO, uri);
+        message.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        PendingIntent pendingIntentMessage = PendingIntent.getActivity(this, 0, message, PendingIntent.FLAG_UPDATE_CURRENT);
+        contentView.setOnClickPendingIntent(R.id.btn_message, pendingIntentMessage);
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+
+                .setSmallIcon(R.drawable.notification)
+                .setTicker("Custom FCM Notification")
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .setCustomBigContentView(contentView);
+
+        contentView.setTextViewText(R.id.tv_title, title);
+        contentView.setTextViewText(R.id.tv_text, body);
+        contentView.setImageViewResource(R.id.iv_icon, R.drawable._white_star);
+
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0, builder.build());
+
+    }
+
+private BroadcastReceiver broadcastReceiverHandler=new BroadcastReceiver() {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String title=intent.getStringExtra("title");
+        String message=intent.getStringExtra("message");
+        createNotification(title,message,context);
+    }
+};
 }
